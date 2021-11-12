4be323f7d5aafec634e023afb17afc747061b2f7
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-13 00:18:35 +0100

    build: added basic tests for the consul snap

--- snaps/consul-homelab/tests.sh
+++ snaps/consul-homelab/tests.sh
@@ -4,4 +4,16 @@ snap list --color=never consul-homelab
 echo '-----------------'
 snap info --color=never consul-homelab
 echo '-----------------'
+echo '-----------------'
 consul-homelab version
+echo '-----------------'
+consul-homelab agent -dev &
+sleep 10s
+consul-homelab members
+echo '-----------------'
+curl localhost:8500/v1/catalog/nodes
+echo '-----------------'
+consul-homelab kv put foo/bar 1
+consul-homelab kv get foo/bar
+echo '-----------------'
+consul-homelab leave
