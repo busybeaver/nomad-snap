cd3d87338023e61cc22ddd5fb9b5988653a33bfe
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-20 23:46:51 +0100

    chore: cleanup

---
+++ snaps/nomad-homelab/tests/set_up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+#sudo mkdir -p "/var/snap/${SERVICE_NAME}/current/config/data"
+#echo "
+#bind_addr = \"{{ GetInterfaceIP \\\"eth0\\\" }}\"
+#data_dir = \"/var/snap/${SERVICE_NAME}/current/config/data\"
+#log_level = \"INFO\"
+#datacenter = \"github_actions\"
+#node_name = \"runner\"
+#server = true
+#bootstrap = true
+#" | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.hcl"
+#sudo snap start "${SERVICE_NAME}.daemon"
+#sleep 10s
