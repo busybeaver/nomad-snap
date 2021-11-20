03d90fbd3ecc40d5adaecb8930ab3cc51cb9b41c
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-20 22:22:10 +0100

    test: use goss for running tests for the consul snap package

--- snaps/consul-homelab/tests/set_up.sh
+++ snaps/consul-homelab/tests/set_up.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -x
 
-sudo mkdir -p "/var/snap/${SERVICE_NAME}/current/config"
-echo 'bind_addr = "{{ GetInterfaceIP \"eth0\" }}"' | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.hcl"
+sudo mkdir -p "/var/snap/${SERVICE_NAME}/current/config/data"
+echo "
+bind_addr = \"{{ GetInterfaceIP \\\"eth0\\\" }}\"
+data_dir = \"/var/snap/${SERVICE_NAME}/current/config/data\"
+log_level = \"INFO\"
+datacenter = \"github_actions\"
+node_name = \"runner\"
+server = true
+" | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.hcl"
