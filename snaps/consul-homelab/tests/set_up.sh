f9badc500e97773d28a76d6d947efdcc24981423
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-20 21:37:20 +0100

    test: use goss for running tests for the consul snap package

--- snaps/consul-homelab/tests/set_up.sh
+++ snaps/consul-homelab/tests/set_up.sh
@@ -2,4 +2,4 @@
 set -x
 
 sudo mkdir -p "/var/snap/${SERVICE_NAME}/current/config"
-echo "{\"dev\": true}" | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.json"
+# echo "dev: true" | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.hcl"
