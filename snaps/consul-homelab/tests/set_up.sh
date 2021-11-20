621ff3b51646cf8114583f274085ba9a1be111c5
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-20 02:09:33 +0100

    chore: testing

--- snaps/consul-homelab/tests/set_up.sh
+++ snaps/consul-homelab/tests/set_up.sh
@@ -2,4 +2,4 @@
 set -x
 
 sudo mkdir -p "/var/snap/${SERVICE_NAME}/current/config"
-sudo echo "{\"dev\": true}" > "/var/snap/${SERVICE_NAME}/current/config/test.json"
+echo "{\"dev\": true}" | sudo tee "/var/snap/${SERVICE_NAME}/current/config/test.json"
