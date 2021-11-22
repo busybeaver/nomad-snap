b23f42c16880de060d760cc75bcf4d17978ba57d
busybeaver <858259+busybeaver@users.noreply.github.com>
2021-11-22 14:23:55 +0100

    chore: testing

--- snaps/consul-homelab/tests/set_up.sh
+++ snaps/consul-homelab/tests/set_up.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -x
 
-sudo mkdir -p /var/snap/"${SERVICE_NAME}"/current/{config,data}/
-ls -la "/var/snap/${SERVICE_NAME}/current/"
+cd /var/snap/"${SERVICE_NAME}"/current/
+sudo mkdir config data
+ls -la
 echo "
 bind_addr = \"{{ GetInterfaceIP \\\"eth0\\\" }}\"
 data_dir = \"/var/snap/${SERVICE_NAME}/current/data\"
@@ -11,6 +12,6 @@ datacenter = \"github_actions\"
 node_name = \"runner\"
 server = true
 bootstrap = true
-" | sudo tee /var/snap/"${SERVICE_NAME}"/current/config/test.hcl
+" | sudo tee ./config/test.hcl
 sudo snap start "${SERVICE_NAME}".daemon
 sleep 10s
