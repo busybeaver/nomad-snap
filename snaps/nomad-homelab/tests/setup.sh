48aa8303cb96689841a5d079da3777b273be5e6d
busybeaver <858259+busybeaver@users.noreply.github.com>
2022-03-20 16:47:53 +0100

    chore: cleanup

--- snaps/nomad-homelab/tests/setup.sh
+++ snaps/nomad-homelab/tests/setup.sh
@@ -5,8 +5,8 @@ cd "/var/snap/${SERVICE_NAME}/current/" || exit 1
 sudo mkdir config data
 
 echo "
-#bind_addr = \"{{ GetInterfaceIP \\\"eth0\\\" }}\"
-bind_addr = \"0.0.0.0\"
+bind_addr = \"{{ GetInterfaceIP \\\"eth0\\\" }}\"
+#bind_addr = \"0.0.0.0\"
 data_dir  = \"/var/snap/${SERVICE_NAME}/current/data\"
 log_level = \"INFO\"
 datacenter = \"github_actions\"
