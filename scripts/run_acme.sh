035974bf2fe2c0f072759e13e6b05f7aa45047c4
busybeaver <858259+busybeaver@users.noreply.github.com>
2022-10-01 20:19:48 +0200

    chore: script cleanup

--- scripts/run_acme.sh
+++ scripts/run_acme.sh
@@ -4,4 +4,4 @@ set -x
 # renovate: datasource=docker depName=neilpang/acme.sh versioning=docker
 IMAGE_VERSION=latest
 
-docker run --rm neilpang/acme.sh:${IMAGE_VERSION} "$@"
+docker run --rm "neilpang/acme.sh:${IMAGE_VERSION}" "$@"
