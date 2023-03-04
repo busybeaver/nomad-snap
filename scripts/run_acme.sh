56595d0fe2628d943b358e49735103f6eec9a106
busybeaver <858259+busybeaver@users.noreply.github.com>
2023-03-04 21:57:53 +0100

    fix(scripts): improvements for running acme.sh

--- scripts/run_acme.sh
+++ scripts/run_acme.sh
@@ -7,4 +7,9 @@ set -x
 # renovate: datasource=docker depName=neilpang/acme.sh versioning=docker
 IMAGE_VERSION=latest@sha256:e9139e0190c7805c3f228fa6dff6a2464771e558285a2efbf204c4a8664c1abd
 
-docker run --rm "neilpang/acme.sh:${IMAGE_VERSION}" "$@"
+docker run \
+  --rm \
+  --net=host \
+  --volume "${ACME_HOME:-$(pwd)/acme.sh}":/acme.sh \
+  --env-file "${ENV_FILE}"
+  "neilpang/acme.sh:${IMAGE_VERSION}" "$@"
