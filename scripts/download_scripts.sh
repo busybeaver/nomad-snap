ca3861a9bd0caf08b0329d9d15d8d65f4437d3cb
busybeaver <858259+busybeaver@users.noreply.github.com>
2023-12-17 18:31:39 +0100

    feat(scripts): verify scripts first before replacing the old files

--- scripts/download_scripts.sh
+++ scripts/download_scripts.sh
@@ -6,15 +6,14 @@ if [ "${DEBUG_SCRIPT:-}" == "TRUE" ]; then
   set -x
 fi
 
-shopt -s extglob
-
 # renovate: datasource=docker depName=bitnami/cosign versioning=docker
 COSIGN_IMAGE_VERSION=1.13.1@sha256:0cede189f264e3939020acd5333d5854fcb60c372cb7f30d7deaefff57ee2965
 
 cd "${CHECKOUT_DIRECTORY:-.}" || exit 1
-rm -rf .[!.]* scripts
 
-git clone --branch main --single-branch --depth 1 --filter=blob:limit=1m "https://github.com/busybeaver/${GIT_REPOSITORY:-homelab-packages}.git" .
+rm -rf ./tmp
+git clone --branch main --single-branch --depth 1 --filter=blob:limit=1m "https://github.com/busybeaver/${GIT_REPOSITORY:-homelab-packages}.git" ./tmp
+cd ./tmp || exit 1
 
 echo "Verify the public sign key: public_key/cosign.pub"
 DOCKER_CONTENT_TRUST=1 docker run \
@@ -40,6 +39,9 @@ DOCKER_CONTENT_TRUST=1 docker run \
 echo "Check the script checksums"
 ( cd ./scripts && sha256sum -c scripts.sha256 )
 
-# ".[!.]*" matches all dot files except "." and files whose name begins with ".." // https://unix.stackexchange.com/a/77313
-# "!(scripts)" matches all non dot files except the scripts folder (requires "extglob")
-rm -rf .[!.]* !(scripts)
+echo "Verification finished and succeeded, replacing old script files"
+cd .. || exit 1
+rm -rf ./scripts
+mv ./tmp/scripts ./scripts
+
+echo "download_scripts.sh script finished successfully"
