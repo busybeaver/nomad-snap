422b988a7efab67c3b2538e2429fa672f8948c20
busybeaver <858259+busybeaver@users.noreply.github.com>
2023-12-19 12:44:09 +0100

    chore(scripts): possibility to skip verification

--- scripts/download_docker_compose.sh
+++ scripts/download_docker_compose.sh
@@ -6,7 +6,7 @@ if [ "${DEBUG_SCRIPT:-}" == "TRUE" ]; then
   set -x
 fi
 
-shopt -s extglob
+echo "download_docker_compose.sh script started at $(date)"
 
 # renovate: datasource=docker depName=mikefarah/yq versioning=docker
 YQ_IMAGE_VERSION=4.40.5@sha256:32be61dc94d0acc44f513ba69d0fc05f1f92c2e760491f2a27e11fc13cde6327
@@ -23,48 +23,54 @@ rm -rf ./tmp
 git clone --branch main --single-branch --depth 1 --filter=blob:limit=1m "git@github.com:${GIT_REPO_PATH}.git" ./tmp
 cd ./tmp || exit 1
 
-# extract images from docker-compose.yaml file
-DOCKER_COMPOSE_IMAGES=$(docker run \
-  --rm \
-  --interactive \
-  --security-opt=no-new-privileges \
-  --cap-drop all \
-  --network none \
-  "mikefarah/yq:${YQ_IMAGE_VERSION}" '.services.[].image' <docker-compose.yaml)
+if [ "${SKIP_VERIFICATION:-}" != "TRUE" ]; then
+  # extract images from docker-compose.yaml file
+  DOCKER_COMPOSE_IMAGES=$(docker run \
+    --rm \
+    --interactive \
+    --security-opt=no-new-privileges \
+    --cap-drop all \
+    --network none \
+    "mikefarah/yq:${YQ_IMAGE_VERSION}" '.services.[].image' <docker-compose.yaml)
 
-printf "The docker-compose.yaml file specifies the following Docker images:\n%s\n\n" "${DOCKER_COMPOSE_IMAGES}"
+  printf "The docker-compose.yaml file specifies the following Docker images:\n%s\n\n" "${DOCKER_COMPOSE_IMAGES}"
 
-echo "Verify the public sign key: public_key/cosign.pub"
-DOCKER_CONTENT_TRUST=1 docker run \
-  --rm \
-  --interactive \
-  --security-opt=no-new-privileges \
-  --cap-drop all \
-  --network none \
-  --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
-  "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /public_key/cosign.pub.sig /public_key/cosign.pub
+  echo "Verify the public sign key: public_key/cosign.pub"
+  DOCKER_CONTENT_TRUST=1 docker run \
+    --rm \
+    --interactive \
+    --security-opt=no-new-privileges \
+    --cap-drop all \
+    --network none \
+    --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
+    "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /public_key/cosign.pub.sig /public_key/cosign.pub
 
-echo "Verify the signature of the docker image and if the image is available and accessible on remote registry"
-for DOCKER_COMPOSE_IMAGE in ${DOCKER_COMPOSE_IMAGES}; do
-  # we can only verify the signature of our own images
-  if [[ "${DOCKER_COMPOSE_IMAGE}" =~ ^ghcr\.io\/busybeaver\/homelab-packages\/.* ]]; then
-    echo "Check signature for image: ${DOCKER_COMPOSE_IMAGE}"
-    DOCKER_CONTENT_TRUST=1 docker run \
-      --rm \
-      --interactive \
-      --security-opt=no-new-privileges \
-      --cap-drop all \
-      --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
-      "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify --key /public_key/cosign.pub --annotations "repo=${GIT_REPO_PATH}" --output text "${DOCKER_COMPOSE_IMAGE}"
-    echo "Check if the image is available and accessible on remote registry: ${DOCKER_COMPOSE_IMAGE}"
-    docker manifest inspect "${DOCKER_COMPOSE_IMAGE}" > /dev/null
-  else
-    echo "Skipping signature and image availability check for image: ${DOCKER_COMPOSE_IMAGE}"
-  fi
-done
+  echo "Verify the signature of the docker image and if the image is available and accessible on remote registry"
+  for DOCKER_COMPOSE_IMAGE in ${DOCKER_COMPOSE_IMAGES}; do
+    # we can only verify the signature of our own images
+    if [[ "${DOCKER_COMPOSE_IMAGE}" =~ ^ghcr\.io\/busybeaver\/homelab-packages\/.* ]]; then
+      echo "Check signature for image: ${DOCKER_COMPOSE_IMAGE}"
+      DOCKER_CONTENT_TRUST=1 docker run \
+        --rm \
+        --interactive \
+        --security-opt=no-new-privileges \
+        --cap-drop all \
+        --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
+        "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify --key /public_key/cosign.pub --annotations "repo=${GIT_REPO_PATH}" --output text "${DOCKER_COMPOSE_IMAGE}"
+      echo "Check if the image is available and accessible on remote registry: ${DOCKER_COMPOSE_IMAGE}"
+      docker manifest inspect "${DOCKER_COMPOSE_IMAGE}" > /dev/null
+    else
+      echo "Skipping signature and image availability check for image: ${DOCKER_COMPOSE_IMAGE}"
+    fi
+  done
+
+  echo "Verification and image availability check finished and succeeded"
+else
+  echo "SKIPPED Verification and image availability check. Use with caution!"
+fi
 
 DOCKER_COMPOSE_FILE="docker-compose.yaml"
-echo "Verification and image availability check finished and succeeded, replacing old ${DOCKER_COMPOSE_FILE} file"
+echo "Replacing old ${DOCKER_COMPOSE_FILE} file"
 cd .. || exit 1
 rm "./${DOCKER_COMPOSE_FILE}"
 mv "./tmp/${DOCKER_COMPOSE_FILE}" "./${DOCKER_COMPOSE_FILE}"
--- scripts/download_scripts.sh
+++ scripts/download_scripts.sh
@@ -6,6 +6,8 @@ if [ "${DEBUG_SCRIPT:-}" == "TRUE" ]; then
   set -x
 fi
 
+echo "download_scripts.sh script started at $(date)"
+
 # renovate: datasource=docker depName=bitnami/cosign versioning=docker
 COSIGN_IMAGE_VERSION=1.13.1@sha256:0cede189f264e3939020acd5333d5854fcb60c372cb7f30d7deaefff57ee2965
 
@@ -15,31 +17,37 @@ rm -rf ./tmp
 git clone --branch main --single-branch --depth 1 --filter=blob:limit=1m "https://github.com/busybeaver/${GIT_REPOSITORY:-homelab-packages}.git" ./tmp
 cd ./tmp || exit 1
 
-echo "Verify the public sign key: public_key/cosign.pub"
-DOCKER_CONTENT_TRUST=1 docker run \
-  --rm \
-  --interactive \
-  --security-opt=no-new-privileges \
-  --cap-drop all \
-  --network none \
-  --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
-  "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /public_key/cosign.pub.sig /public_key/cosign.pub
+if [ "${SKIP_VERIFICATION:-}" != "TRUE" ]; then
+  echo "Verify the public sign key: public_key/cosign.pub"
+  DOCKER_CONTENT_TRUST=1 docker run \
+    --rm \
+    --interactive \
+    --security-opt=no-new-privileges \
+    --cap-drop all \
+    --network none \
+    --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
+    "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /public_key/cosign.pub.sig /public_key/cosign.pub
 
-echo "Verify the checksum file: scripts/scripts.sha256"
-DOCKER_CONTENT_TRUST=1 docker run \
-  --rm \
-  --interactive \
-  --security-opt=no-new-privileges \
-  --cap-drop all \
-  --network none \
-  --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
-  --volume "${SCRIPTS_DIRECTORY:-$(pwd)/scripts}":/scripts:ro \
-  "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /scripts/scripts.sha256.sig /scripts/scripts.sha256
+  echo "Verify the checksum file: scripts/scripts.sha256"
+  DOCKER_CONTENT_TRUST=1 docker run \
+    --rm \
+    --interactive \
+    --security-opt=no-new-privileges \
+    --cap-drop all \
+    --network none \
+    --volume "${PUBLIC_KEY_DIRECTORY:-$(pwd)/public_key}":/public_key:ro \
+    --volume "${SCRIPTS_DIRECTORY:-$(pwd)/scripts}":/scripts:ro \
+    "bitnami/cosign:${COSIGN_IMAGE_VERSION}" verify-blob --key /public_key/cosign.pub --signature /scripts/scripts.sha256.sig /scripts/scripts.sha256
 
-echo "Check the script checksums"
-(cd ./scripts && sha256sum -c scripts.sha256)
+  echo "Check the script checksums"
+  (cd ./scripts && sha256sum -c scripts.sha256)
 
-echo "Verification finished and succeeded, replacing old script files"
+  echo "Verification finished and succeeded"
+else
+  echo "SKIPPED Verification. Use with caution!"
+fi
+
+echo "Replacing old script files"
 cd .. || exit 1
 rm -rf ./scripts
 mv ./tmp/scripts ./scripts
