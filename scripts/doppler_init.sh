399a6410a731447a93f8b10828433ece0870449a
busybeaver <858259+busybeaver@users.noreply.github.com>
2023-12-24 17:12:11 +0100

    feat(scripts): script to install doppler

--- scripts/doppler_init.sh
+++ scripts/doppler_init.sh
@@ -6,4 +6,17 @@ if [ "${DEBUG_SCRIPT:-}" == "TRUE" ]; then
   set -x
 fi
 
-curl -Ls --proto "=https" --tlsv1.3 --http3 --retry 3 https://cli.doppler.com/install.sh | sudo sh
+# renovate: datasource=github-releases depName=DopplerHQ/cli versioning=loose
+DOPPLER_VERSION="3.66.5"
+
+DOPPLER_TEMP_INSTALL_DIRECTORY="/tmp/doppler-install"
+
+mkdir "${DOPPLER_TEMP_INSTALL_DIRECTORY}"
+cd "${DOPPLER_TEMP_INSTALL_DIRECTORY}"
+curl -Ls --proto "=https" --tlsv1.3 --retry 3 "https://github.com/DopplerHQ/cli/releases/download/${DOPPLER_VERSION}/doppler_${DOPPLER_VERSION}_linux_amd64.tar.gz" > doppler_linux_arm64.tar.gz
+tar -xvzf doppler_linux_arm64.tar.gz
+
+chmod +x "${DOPPLER_TEMP_INSTALL_DIRECTORY}/doppler"
+sudo mv "${DOPPLER_TEMP_INSTALL_DIRECTORY}/doppler" /usr/local/bin/doppler
+
+rm -rf "${DOPPLER_TEMP_INSTALL_DIRECTORY}"
