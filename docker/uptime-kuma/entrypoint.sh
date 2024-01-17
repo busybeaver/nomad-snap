3da8281a091c086fe628f34cd99c3747d35b4347
busybeaver <858259+busybeaver@users.noreply.github.com>
2024-01-17 23:22:47 +0100

    fix(docker): formatted shell script

--- docker/uptime-kuma/entrypoint.sh
+++ docker/uptime-kuma/entrypoint.sh
@@ -4,21 +4,21 @@
 set -e
 DATADIR=/app/data
 
-files_ownership () {
+files_ownership() {
   # Check if the $DATADIR folder is owned by the user invoking the container
-  if [ $(stat -c%u "$DATADIR") != $(id -u) ]; then
+  if [ "$(stat -c%u "$DATADIR")" != "$(id -u)" ]; then
     echo "File ownership incorrect, attempting to fix."
     chown -hRc "$(id -u)":"$(id -g)" $DATADIR || echo "ERROR: Failed to set file ownership. Please run 'sudo chown -R $(id -u):$(id -g) /path/to/container/volume' to resolve." && exit 1
     echo "File ownership fix succesful! Continuing."
   fi
 
   # Checks for R/W permissions
-  if [ $(stat -c%a "$DATADIR") -ne 770 ]; then
+  if [ "$(stat -c%a "$DATADIR")" -ne 770 ]; then
     echo "Directory permissions incorrect, attempting to fix."
     find $DATADIR -type d -exec chmod 770 {} \;
 
     # Re-run the check
-    if [ $(stat -c%a "$DATADIR") -ne 770 ]; then
+    if [ "$(stat -c%a "$DATADIR")" -ne 770 ]; then
       echo "ERROR: Failed to set file permissions. Please run 'sudo find /path/to/container/volume -type d chmod 770 {} \;' to resolve."
       exit 1
     fi
@@ -26,12 +26,12 @@ files_ownership () {
   fi
 
   # Check the R/W permissions on the files
-  if [ $(stat -c%a "$DATADIR"/* | head -n 1) != 640 ]; then
+  if [ "$(stat -c%a "$DATADIR"/* | head -n 1)" != 640 ]; then
     echo "File permissions incorrect. Attempting to fix."
     find $DATADIR -type f -exec chmod 640 {} \;
 
     # Re-run the check
-    if [ $(stat -c%a "$DATADIR"/* | head -n 1) != 640 ]; then
+    if [ "$(stat -c%a "$DATADIR"/* | head -n 1)" != 640 ]; then
       echo "ERROR: Failed to set file permissions. Please run 'sudo find /path/to/container/volume -type f chmod 640 {} \;' to resolve."
       exit 1
     fi
@@ -46,7 +46,7 @@ files_ownership
 echo "==> Starting application as user: $(id -u) ($USER) and group $(id -g)"
 
 # --clear-groups Clear supplementary groups.
-if [ $(id -u) -eq 0 ]; then
+if [ "$(id -u)" -eq 0 ]; then
   # We're running as root, so we can use setpriv without problems.
   exec setpriv --reuid "$PUID" --regid "$PGID" --clear-groups "$@"
 else
