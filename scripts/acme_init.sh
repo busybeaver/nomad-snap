11853937d15081580cf7d9c7c9e5bcf2cd5b428e
busybeaver <858259+busybeaver@users.noreply.github.com>
2024-05-26 21:17:08 +0200

    chore(scripts): refactored acme_init script

--- scripts/acme_init.sh
+++ scripts/acme_init.sh
@@ -13,6 +13,8 @@ echo "acme_init.sh script started at $(date)"
 
 HOMELAB_PACKAGES_CONFIG="${HOMELAB_PACKAGES_CONFIG_FOLDER:-~/homelab-packages-config}"
 
+# note: unifi is running ssl cert updates on its own
+
 # general setup
 export CERT_DNS="dns_cf"
 export ACME_DIRECTORY_RESOURCE="letsencrypt"
@@ -20,37 +22,64 @@ export ACME_HOME="${HOMELAB_PACKAGES_CONFIG}/acme.sh"
 export GLOBAL_ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/cf.env"
 
 # synology nas
-export CERT_DOMAIN="nas1.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/syno.env"
-# bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}"
-# bash scripts/acme_run.sh --deploy --domain "$CERT_DOMAIN" --deploy-hook synology_dsm --insecure
+if [ "${INIT_SYNOLOGY:-}" == "TRUE" ]; then
+  export CERT_DOMAIN_1="nas1.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_2="synology1.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/syno.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --deploy --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --deploy-hook synology_dsm --insecure
+fi
+
+# fritz!box
+if [ "${INIT_FRITZ_BOX:-}" == "TRUE" ]; then
+  export CERT_DOMAIN_1="fritz-box.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_2="router.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/fritz-box.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --deploy --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --deploy-hook fritzbox --insecure
+fi
 
 # adguard
-export CERT_DOMAIN_1="adguard1.mollner.cloud"
-export CERT_DOMAIN_2="dns1.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/adguard.env"
-# bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN_1" --domain "$CERT_DOMAIN_2" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}" --post-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/adguard/conf/certs/cert.key && cp \$CERT_PATH /volume_base_directory/adguard/conf/certs/cert.cer && chown -R ${ADGUARD_USER}:${DOCKER_USER_GROUP} /volume_base_directory/adguard/conf/certs" --force
+if [ "${INIT_ADGUARD:-}" == "TRUE" ]; then
+  export CERT_DOMAIN_1="adguard1.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_2="dns1.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/adguard.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+    --renew-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/adguard/conf/certs/cert.key && cp \$CERT_PATH /volume_base_directory/adguard/conf/certs/cert.cer && chown -R ${ADGUARD_USER}:${DOCKER_USER_GROUP} /volume_base_directory/adguard/conf/certs"
+fi
 
 # uptime-kuma
-export CERT_DOMAIN="uptime.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/uptime-kuma.env"
-bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}" --post-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/uptime-kuma/cert/cert.key && cp \$CERT_PATH /volume_base_directory/uptime-kuma/cert/cert.cer && chown -R ${UPTIME_KUMA_USER}:${DOCKER_USER_GROUP} /volume_base_directory/uptime-kuma/cert" --force
+if [ "${INIT_UPTIME_KUMA:-}" == "TRUE" ]; then
+  export CERT_DOMAIN_1="uptime.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_2="status.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_3="health.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/uptime-kuma.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --domain "${CERT_DOMAIN_3}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+    --renew-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/uptime-kuma/cert/cert.key && cp \$CERT_PATH /volume_base_directory/uptime-kuma/cert/cert.cer && chown -R ${UPTIME_KUMA_USER}:${DOCKER_USER_GROUP} /volume_base_directory/uptime-kuma/cert"
+fi
 
 # homebridge
-export CERT_DOMAIN="homebridge.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/homebridge.env"
-# bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}"
+if [ "${INIT_HOMEBRIDGE:-}" == "TRUE" ]; then
+  export CERT_DOMAIN="homebridge.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/homebridge.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+fi
 
 # n8n
-export CERT_DOMAIN="n8n.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/n8n.env"
-# bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}"
+if [ "${INIT_N8N:-}" == "TRUE" ]; then
+  export CERT_DOMAIN="n8n.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/n8n.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+fi
 
 # node-red
-export CERT_DOMAIN="node-red.mollner.cloud"
-export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/node-red.env"
-# bash scripts/acme_run.sh --issue --domain "$CERT_DOMAIN" --dns "$CERT_DNS" --server "${ACME_DIRECTORY_RESOURCE}"
-
+if [ "${INIT_NODE_RED:-}" == "TRUE" ]; then
+  export CERT_DOMAIN_1="node-red.${ROOT_DOMAIN}"
+  export CERT_DOMAIN_2="status.${ROOT_DOMAIN}"
+  export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/node-red.env"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+    --renew-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/node-red/cert/cert.key && cp \$CERT_PATH /volume_base_directory/node-red/cert/cert.cer && chown -R ${NODE_RED_USER}:${DOCKER_USER_GROUP} /volume_base_directory/node-red/cert"
+fi
 shopt -u nocasematch
 
 echo "acme_init.sh script finished successfully at $(date)"
