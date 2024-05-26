b0f12df25f4f00ee769725f78e0f00ef379c208a
busybeaver <858259+busybeaver@users.noreply.github.com>
2024-05-26 22:42:20 +0200

    chore(scripts): refactored acme_init script

--- scripts/acme_init.sh
+++ scripts/acme_init.sh
@@ -28,7 +28,7 @@ if [ "${INIT_SYNOLOGY^^}" == "TRUE" ]; then
   export CERT_DOMAIN_1="nas1.${ROOT_DOMAIN}"
   export CERT_DOMAIN_2="synology1.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/syno.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-}
   bash scripts/acme_run.sh --deploy --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --deploy-hook synology_dsm --insecure
   echo "--- Finished certificate renewal setup for Synology 1 ---"
 fi
@@ -39,7 +39,7 @@ if [ "${INIT_FRITZ_BOX^^}" == "TRUE" ]; then
   export CERT_DOMAIN_1="fritz-box.${ROOT_DOMAIN}"
   export CERT_DOMAIN_2="router.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/fritz-box.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-}
   bash scripts/acme_run.sh --deploy --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --deploy-hook fritzbox --insecure
   echo "--- Finished certificate renewal setup for Fritz!Box ---"
 fi
@@ -50,7 +50,7 @@ if [ "${INIT_ADGUARD^^}" == "TRUE" ]; then
   export CERT_DOMAIN_1="adguard1.${ROOT_DOMAIN}"
   export CERT_DOMAIN_2="dns1.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/adguard.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-} \
     --post-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/adguard/conf/certs/cert.key && cp \$CERT_PATH /volume_base_directory/adguard/conf/certs/cert.cer && chown -R ${ADGUARD_USER}:${DOCKER_USER_GROUP} /volume_base_directory/adguard/conf/certs"
   echo "--- Finished certificate renewal setup for Adguard 1 ---"
 fi
@@ -62,7 +62,7 @@ if [ "${INIT_UPTIME_KUMA^^}" == "TRUE" ]; then
   export CERT_DOMAIN_2="status.${ROOT_DOMAIN}"
   export CERT_DOMAIN_3="health.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/uptime-kuma.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --domain "${CERT_DOMAIN_3}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --domain "${CERT_DOMAIN_3}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-} \
     --post-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/uptime-kuma/cert/cert.key && cp \$CERT_PATH /volume_base_directory/uptime-kuma/cert/cert.cer && chown -R ${UPTIME_KUMA_USER}:${DOCKER_USER_GROUP} /volume_base_directory/uptime-kuma/cert"
   echo "--- Finished certificate renewal setup for Uptime Kuma ---"
 fi
@@ -72,7 +72,7 @@ if [ "${INIT_HOMEBRIDGE^^}" == "TRUE" ]; then
   echo "--- Initialize certificate renewal setup for Homebridge ---"
   export CERT_DOMAIN="homebridge.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/homebridge.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-}
   echo "--- Finished certificate renewal setup for Homebridge ---"
 fi
 
@@ -81,7 +81,7 @@ if [ "${INIT_N8N^^}" == "TRUE" ]; then
   echo "--- Initialize certificate renewal setup for N8N ---"
   export CERT_DOMAIN="n8n.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/n8n.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}"
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-}
   echo "--- Finished certificate renewal setup for N8N ---"
 fi
 
@@ -91,7 +91,7 @@ if [ "${INIT_NODE_RED^^}" == "TRUE" ]; then
   export CERT_DOMAIN_1="node-red.${ROOT_DOMAIN}"
   export CERT_DOMAIN_2="automation.${ROOT_DOMAIN}"
   export ENV_FILE="${HOMELAB_PACKAGES_CONFIG}/env_files/node-red.env"
-  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" \
+  bash scripts/acme_run.sh --issue --domain "${CERT_DOMAIN_1}" --domain "${CERT_DOMAIN_2}" --dns "${CERT_DNS}" --server "${ACME_DIRECTORY_RESOURCE}" ${FORCE:-} \
     --post-hook "set -x && cp \$CERT_KEY_PATH /volume_base_directory/node-red/cert/cert.key && cp \$CERT_PATH /volume_base_directory/node-red/cert/cert.cer && chown -R ${NODE_RED_USER}:${DOCKER_USER_GROUP} /volume_base_directory/node-red/cert"
   echo "--- Finished certificate renewal setup for Node-RED ---"
 fi
